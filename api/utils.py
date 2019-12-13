19e0f40c7d19d9c743e713a05d81b60134491f74
SamaneriVimala <samaneri.vimala@gmail.com>
2019-12-13 10:17:11 +0100

    removing Travis errors

--- api/utils.py
+++ api/utils.py
@@ -5,7 +5,7 @@ Various utilities for interacting with data in API queries.
 import re
 from typing import List
 
-from .db_queries import ( QUERY_ONE_COLLECTION )
+from .db_queries import (QUERY_ONE_COLLECTION)
 from .db_connection import get_db
 
 COLLECTION_PATTERN = r"^(pli-tv-b[ui]-vb|[A-Z]+[0-9]+|[a-z\-]+)"
@@ -31,37 +31,20 @@ def get_collection_files_regex(limit_collection, language) -> List:
     :return: The regular expressions to test if resource belongs to a given collection
     if a collection is prefixed with !, we exclude it from the results!
     """
-
-    """
-    First scan the limit_collection to determine if it is a a collection of multiple categories
-    and if so, find all the categories in that collection and add them to a new list.
-    """
     new_limit_collection = []
     for file in limit_collection:
         if re.search("[a-z]+_[A-Z][a-z]+$", file):
-            try:
-                query = get_db().AQLQuery(
-                    query=QUERY_ONE_COLLECTION,
-                    bindVars={
-                        "collectionkey": file.replace("!", "")
-                    },
-                )
-                for item in query.result[0]:
-                    if "!" not in file:
-                        new_limit_collection.append(item)
-                    else:
-                        new_limit_collection.append("!"+item)
-
-            except DocumentNotFoundError as error:
-                print(error)
-                raise HTTPException(status_code=404, detail="Item not found")
-            except AQLQueryError as error:
-                print("AQLQueryError: ", error)
-                raise HTTPException(status_code=400, detail=error.errors)
-            except KeyError as error:
-                print("KeyError: ", error)
-                raise HTTPException(status_code=400)
-
+            query = get_db().AQLQuery(
+                query=QUERY_ONE_COLLECTION,
+                bindVars={
+                    "collectionkey": file.replace("!", "")
+                },
+            )
+            for item in query.result[0]:
+                if "!" not in file:
+                    new_limit_collection.append(item)
+                else:
+                    new_limit_collection.append("!"+item)
         else:
             new_limit_collection.append(file)
 
