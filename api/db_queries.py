729ba3198d14c34790a0ec6839404d62494d1ec4
Sebastian <basti@slayer>
2020-01-23 11:08:37 +0900

    Small fix for a bug in ArangoDB 3.6.

--- api/db_queries.py
+++ api/db_queries.py
@@ -220,6 +220,7 @@ let parallels =  (
                     )
                     LET filternr2 = (@limitcollection_negative != []) ? POSITION(filtertest2, true) : false
                     FILTER filternr2 == false
+                     LIMIT 100000
                     RETURN { root_offset_beg: p.root_offset_beg,
                              root_offset_end: p.root_offset_end,
                              root_segnr : p.root_segnr,
