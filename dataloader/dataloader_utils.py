7534d6269e8b097540987c4f3759c89f3394e316
ayya-vimala <ayya.vimala@gmail.com>
2020-03-08 10:06:46 +0100

    width setting for FireFox

--- dataloader/dataloader_utils.py
+++ dataloader/dataloader_utils.py
@@ -82,7 +82,7 @@ def should_download_file(file_lang: str, file_name: str) -> bool:
     Limit source file set size to speed up loading process
     Can be controlled with the `LIMIT` environment variable.
     """
-    if file_lang == LANG_TIBETAN:
+    if file_lang == LANG_TIBETAN and file_name.startswith("T06"):
         return True
     else:
         return False
