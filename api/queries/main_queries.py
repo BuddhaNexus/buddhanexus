0b1dc261e1ca03bb58d56ff9cc67a6f249c1c5f5
ayya-vimala <ayya.vimala@gmail.com>
2020-09-10 14:15:26 +0200

    small mistake

--- api/queries/main_queries.py
+++ api/queries/main_queries.py
@@ -40,7 +40,6 @@ FOR segment IN 1..1 OUTBOUND concat("files/", @filename) GRAPH 'files_segments'
 
     LET seg_parallels = (
         FOR p IN 1..1 OUTBOUND segment GRAPH 'files_segments'
-            FILTER REGEX_MATCHES(p.root_segnr[0], @start_folio, true)
             FILTER p.score >= @score
             FILTER p.par_length >= @parlength
             FILTER p["co-occ"] <= @coocc
