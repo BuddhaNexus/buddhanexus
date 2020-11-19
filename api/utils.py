a41e1f7a23411c529d87058a27cee35a5f476e44
Sebastian Nehrdich <nehrdbsd@gmail.com>
2020-11-19 14:36:49 +0100

    Update utils.py

--- api/utils.py
+++ api/utils.py
@@ -36,7 +36,6 @@ def create_cleaned_limit_collection(limit_collection) -> List:
     new_limit_collection = []
     for file in limit_collection:
         if re.search("([a-z]+_[A-Z][a-z]+[a-z1-2EL-]+$)|tib_Ny", file):
-            print("DETECTED",file)
             query = get_db().AQLQuery(
                 query=menu_queries.QUERY_ONE_COLLECTION,
                 bindVars={"collectionkey": file.replace("!", "")},
@@ -47,7 +46,6 @@ def create_cleaned_limit_collection(limit_collection) -> List:
                 else:
                     new_limit_collection.append("!" + item)
         else:
-            print("NOT DETECTED",file)
             new_limit_collection.append(file)
 
     return new_limit_collection
