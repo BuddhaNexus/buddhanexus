f8ec7a24da3d99804f9c89298ad9f5110bdbce9a
trebuh <hubert.dworczynski@gmail.com>
2022-10-30 08:18:26 +0100

    add background images for all source languages

--- components/layout/PageContainer.tsx
+++ components/layout/PageContainer.tsx
@@ -1,34 +1,53 @@
 import type { FC, PropsWithChildren } from "react";
 import { Container } from "@mui/material";
-import bgPali from "public/assets/images/pli_bg_optimized-min.jpeg";
-import type { SourceLanguage } from "utils/constants";
+import bgChn from "@public/assets/images/bg_chn_upscaled.jpeg";
+import bgPli from "@public/assets/images/bg_pli_upscaled.jpeg";
+import bgSkt from "@public/assets/images/bg_skt_upscaled.jpeg";
+import bgTib from "@public/assets/images/bg_tib_upscaled.jpeg";
+import { SourceLanguage } from "utils/constants";
+
+const BgImageSrcs = {
+  [SourceLanguage.TIBETAN]: bgTib.src,
+  [SourceLanguage.CHINESE]: bgChn.src,
+  [SourceLanguage.SANSKRIT]: bgSkt.src,
+  [SourceLanguage.PALI]: bgPli.src,
+};
+
+const BgImageOpacities = {
+  [SourceLanguage.TIBETAN]: 0.1,
+  [SourceLanguage.CHINESE]: 0.35,
+  [SourceLanguage.SANSKRIT]: 0.2,
+  [SourceLanguage.PALI]: 0.2,
+};
 
 interface Props extends PropsWithChildren {
   sourceLanguage?: SourceLanguage;
 }
 
-export const PageContainer: FC<Props> = ({ children, sourceLanguage }) => (
-  <>
-    {sourceLanguage && (
+export const PageContainer: FC<Props> = ({ children, sourceLanguage }) => {
+  return (
+    <>
+      {sourceLanguage && (
+        <Container
+          maxWidth={false}
+          sx={{
+            background: `url(${BgImageSrcs[sourceLanguage]})`,
+            height: "100%",
+            width: "100%",
+            position: "fixed",
+            backgroundSize: "contain",
+            zIndex: -1,
+            opacity: BgImageOpacities[sourceLanguage],
+          }}
+        />
+      )}
       <Container
-        maxWidth={false}
-        sx={{
-          background: `url(${bgPali.src})`,
-          height: "100%",
-          width: "100%",
-          position: "fixed",
-          backgroundSize: "contain",
-          zIndex: -1,
-          opacity: 0.3,
-        }}
-      />
-    )}
-    <Container
-      component="main"
-      maxWidth="md"
-      sx={{ pt: 8, flex: 1, display: "flex", flexDirection: "column" }}
-    >
-      {children}
-    </Container>
-  </>
-);
+        component="main"
+        maxWidth="md"
+        sx={{ pt: 8, flex: 1, display: "flex", flexDirection: "column" }}
+      >
+        {children}
+      </Container>
+    </>
+  );
+};
