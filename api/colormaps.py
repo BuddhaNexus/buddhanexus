bf3c23bceb255b7afa9ac51ccf9a377c1a3c284d
Sebastian <basti@slayer>
2023-01-25 13:43:17 +0100

    Simplify colormap api response

--- api/colormaps.py
+++ api/colormaps.py
@@ -1,5 +1,21 @@
 import time 
 
+def create_segmented_text(text, colormap):
+    result_segments = []
+    current_segment = ""
+    last_color = colormap[0]
+    for i in range(len(text)):
+        current_color = colormap[i]
+        if current_color != last_color:
+            result_segments.append({"text": current_segment,
+                                   "highlightColor": last_color})
+            current_segment = ""
+        current_segment += text[i]
+        last_color = current_color
+    result_segments.append({"text": current_segment,
+                            "highlightColor": last_color})
+    return result_segments
+
 def calculate_color_maps_text_view(data):
     time_before = time.time()
     textleft = data['textleft']
@@ -27,14 +43,14 @@ def calculate_color_maps_text_view(data):
                 current_colormap[start:end] = [v+1 for v in current_colormap[start:end]]
             else:
                 print("Warning: parallel id", id, "not found in parallels_dict")
-        entry['color_map'] = current_colormap
+        entry['segtext'] = create_segmented_text(entry['segtext'], current_colormap)
+        print("entry", entry)
         del(entry['parallel_ids'])
     time_after = time.time()
     print("Time to calculate color maps: ", time_after - time_before)
     return textleft
 
 def calculate_color_maps_table_view(data):
-
     for entry in data:
         join_element_root = ""
         join_element_par = ""
@@ -42,6 +58,7 @@ def calculate_color_maps_table_view(data):
             join_element_root = " "
         if entry['tgt_lang'] == "tib":
             join_element_par = " "
+
         root_fulltext = join_element_root.join(entry['root_seg_text'])
         root_colormap = [0] * len(root_fulltext)
         root_end = len(root_fulltext) - (len(entry['root_seg_text'][-1]) - entry['root_offset_end']) 
@@ -49,45 +66,26 @@ def calculate_color_maps_table_view(data):
             root_end = len(root_fulltext)
         root_start = entry['root_offset_beg']
         root_colormap[root_start:root_end] = [1] * (root_end - root_start)
-        entry['root_color_map'] = root_colormap
+        root_fulltext = create_segmented_text(root_fulltext, root_colormap)
         entry['root_fulltext'] = root_fulltext
 
         par_fulltext = join_element_par.join(entry['par_segment'])
         par_colormap = [0] * len(par_fulltext)
-
-
         par_end = len(par_fulltext) - (len(entry['par_segment'][-1]) - entry['par_offset_end'])
         if par_end > len(par_fulltext):
             par_end = len(par_fulltext)
         par_start = entry['par_offset_beg']
         par_colormap[par_start:par_end] = [1] * (par_end - par_start)
-        print(entry)
-        print("LEN PAR FULLTEXT", len(par_fulltext))
-        print("LEN PAR COLORMAP", len(par_colormap))
-        print("PAR END", par_end)
-        entry['par_color_map'] = par_colormap
+        par_fulltext = create_segmented_text(par_fulltext, par_colormap)
         entry['par_fulltext'] = par_fulltext
 
-
-        print("PAR FULLTEXT", entry['par_fulltext'])
-        par_test_string = ""
-        for c in range(len(par_colormap)):
-            if par_colormap[c] == 1:
-                par_test_string += par_fulltext[c]
-        print("PAR TEST STRING", par_test_string)
-        
-        root_test_string = ""
-        for c in range(len(root_colormap)):
-            if root_colormap[c] == 1:
-                root_test_string += root_fulltext[c]
-        print("ROOT TEST STRING", root_test_string)
-        print("ROOT FULLTEXT", entry['root_fulltext'], "\n\n")
         del(entry['par_segment'])
         del(entry['root_seg_text'])
         del(entry['root_offset_beg'])
         del(entry['root_offset_end'])
         del(entry['par_offset_beg'])
         del(entry['par_offset_end'])
+        del(entry['par_pos_beg'])
     return data
 
 
