63cb92e7699db67a015dd49ff33f371bcc8c9f23
aminahbl <aminahbl.github@37e.uk>
2023-02-06 14:47:41 +0100

    fix PartnerInstitution imgs

--- components/static/PartnerInstitution.tsx
+++ components/static/PartnerInstitution.tsx
@@ -14,15 +14,31 @@ function PartnerInstitution({ name, href, alt, src }: Props) {
     <Box
       component="figure"
       maxWidth="sm"
-      sx={{ display: "flex", flexDirection: "column" }}
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        mx: {
+          xs: 1,
+          sm: 2,
+        },
+      }}
     >
       <Typography variant="h5" component="figcaption" sx={{ mb: 2 }}>
         {name}
       </Typography>
-      <Paper elevation={3}>
+      <Paper
+        sx={{
+          position: "relative",
+          height: {
+            xs: "170px",
+            sm: "300px",
+          },
+          width: "100%",
+        }}
+        elevation={3}
+      >
         <Link href={href} target="_blank" rel="noopener noreferrer">
-          {/* <Image src={src} alt={alt} fill={true} placeholder="blur" /> */}
-          <Image src={src} alt={alt} width={500} height={340} />
+          <Image src={src} alt={alt} fill={true} placeholder="blur" />
         </Link>
       </Paper>
       <br />
