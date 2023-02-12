117b4c85f19acaa6b2a6c02518aa0cc36e8888b5
Hubert Dworczyński <7022085+TrebuhD@users.noreply.github.com>
2023-02-12 21:56:21 +0100

    fetch data for source text browser in sidebar

--- components/db/SourceTextBrowserTree.tsx
+++ components/db/SourceTextBrowserTree.tsx
@@ -3,8 +3,9 @@ import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import { TreeItem } from "@mui/lab";
 import TreeView from "@mui/lab/TreeView";
+import { CircularProgress } from "@mui/material";
 import { useQuery } from "@tanstack/react-query";
-import type { ApiSourceTextBrowserData } from "types/api/common";
+import type { SourceTextBrowserData } from "types/api/sourceTextBrowser";
 import { DbApi } from "utils/api/dbApi";
 
 // https://buddhanexus.kc-tbts.uni-hamburg.de/api/menus/sidebar/pli
@@ -13,12 +14,14 @@ export function SourceTextBrowserTree() {
   const { sourceLanguage } = useDbQueryParams();
 
   // TODO: add error handling
-  const { data, isLoading } = useQuery<ApiSourceTextBrowserData>({
+  const { isLoading } = useQuery<SourceTextBrowserData>({
     queryKey: DbApi.SidebarSourceTexts.makeQueryKey(sourceLanguage),
     queryFn: () => DbApi.SidebarSourceTexts.call(sourceLanguage),
   });
 
-  console.log({ data });
+  if (isLoading) {
+    return <CircularProgress />;
+  }
 
   return (
     <TreeView
--- utils/api/sidebarSourceTexts.ts
+++ utils/api/sidebarSourceTexts.ts
@@ -1,19 +1,37 @@
 // source text collections
+import type {
+  ApiSourceTextBrowserData,
+  SourceTextBrowserData,
+} from "types/api/sourceTextBrowser";
 import type { SourceLanguage } from "utils/constants";
 
 import { API_ROOT_URL } from "./constants";
+
+function parseSourceTextCollectionData(
+  data: ApiSourceTextBrowserData
+): SourceTextBrowserData {
+  return data.navigationmenudata.map(({ collection, categories }) => ({
+    collection,
+    categories: categories.map(
+      ({ files, categorydisplayname, categoryname }) => ({
+        files: files.map(
+          ({ textname, filename, available_lang, displayname }) => ({
+            textName: textname,
+            displayName: displayname,
+            fileName: filename,
+            availableLanguages: available_lang,
+          })
+        ),
+        name: categoryname,
+        displayName: categorydisplayname,
+      })
+    ),
+  }));
+}
 
 export async function getSourceTextCollections(language: SourceLanguage) {
   const res = await fetch(`${API_ROOT_URL}/menus/sidebar/${language}`);
   const response = await res.json();
 
-  console.log({ response });
-
-  return response.result.map((collection: SourceTextCollectionApiData) => ({
-    label: menuItem.search_field,
-    id: menuItem.search_field,
-    name: menuItem.displayName,
-    textName: menuItem.textname,
-    category: menuItem.category,
-  }));
+  return parseSourceTextCollectionData(response);
 }
