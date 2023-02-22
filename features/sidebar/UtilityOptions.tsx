874ff8aaef7ae02664f102eee97d6c07a5fba1c1
aminahbl <aminahbl.github@37e.uk>
2023-02-22 22:17:24 +0100

    draft utility options functionality with snackbar

--- features/sidebar/UtilityOptions.tsx
+++ features/sidebar/UtilityOptions.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useTranslation } from "next-i18next";
 import { currentDbViewAtom } from "@components/db/DbViewSelector";
 import { useDbQueryParams } from "@components/hooks/useDbQueryParams";
@@ -14,15 +14,12 @@ import {
   ListItemButton,
   ListItemIcon,
   ListItemText,
+  Snackbar,
   Typography,
 } from "@mui/material";
 import type { OverridableComponent } from "@mui/material/OverridableComponent";
-import {
-  FolioOption,
-  TextScriptOption,
-} from "features/sidebar/settingComponents";
-import { StandinFilter } from "features/sidebar/Sidebar";
 import { useAtomValue } from "jotai";
+import { API_ROOT_URL } from "utils/api/constants";
 import {
   isOnlyNull,
   isSettingOmitted,
@@ -30,30 +27,85 @@ import {
   type UtilityOption,
 } from "utils/dbUISettings";
 
+type SnackbarState = Record<UtilityOption, boolean>;
+type SnackbarStateSetter = [
+  SnackbarState,
+  React.Dispatch<React.SetStateAction<SnackbarState>>
+];
+
+const onDownload = async (
+  fileName: string,
+  snackbarState: SnackbarStateSetter
+) => {
+  const [snackbarIsOpen, setSnackbarIsOpen] = snackbarState;
+
+  // https://buddhanexus.net/chn/T01n0004_download.xlsx
+  // https://buddhanexus.net/download/dn6_download.xlsx
+
+  const url = `${API_ROOT_URL}/download/${fileName}_download.xlsx`;
+  const testUrlValidity = await fetch(url);
+
+  if (testUrlValidity.status === 404) {
+    setSnackbarIsOpen({ ...snackbarIsOpen, download: true });
+  } else {
+    const link = document.createElement("a");
+    link.download = `${fileName}.xlsx`;
+    link.href = url;
+    link.click();
+  }
+};
+
+const onCopyQueryTitle = (
+  fileName: string,
+  snackbarState: SnackbarStateSetter
+) => {
+  const [snackbarIsOpen, setSnackbarIsOpen] = snackbarState;
+  setSnackbarIsOpen({ ...snackbarIsOpen, copyQueryTitle: true });
+  // eslint-disable-next-line @typescript-eslint/no-floating-promises
+  navigator.clipboard.writeText(fileName);
+};
+
+const onCopyQueryLink = (
+  fileName: string,
+  snackbarState: SnackbarStateSetter
+) => {
+  const [snackbarIsOpen, setSnackbarIsOpen] = snackbarState;
+  setSnackbarIsOpen({ ...snackbarIsOpen, copyQueryLink: true });
+  // eslint-disable-next-line @typescript-eslint/no-floating-promises
+  navigator.clipboard.writeText(window.location.toString());
+};
+
+const onEmailQueryLink = (
+  fileName: string,
+  snackbarState: SnackbarStateSetter
+) => {
+  const [snackbarIsOpen, setSnackbarIsOpen] = snackbarState;
+  setSnackbarIsOpen({ ...snackbarIsOpen, emailQueryLink: true });
+
+  const subject = `BuddhaNexus serach results - ${fileName.toUpperCase()}`;
+  const body = `Here is a link to search results for ${fileName.toUpperCase()}: ${window.location.toString()}`;
+
+  const link = document.createElement("a");
+  link.href = `mailto:?subject=${subject}&body=${body}`;
+  link.click();
+};
+
 const utilityOptionComponents: [
   UtilityOption,
-  React.ElementType,
+  (fileName: string, snackbarState: SnackbarStateSetter) => void,
   OverridableComponent<SvgIconTypeMap>
 ][] = [
-  ["download", TextScriptOption, FileDownloadIcon],
-  ["copyQueryTitle", FolioOption, LocalOfferOutlinedIcon],
-  [
-    "copyQueryLink",
-    () => StandinFilter("showAndPositionSegmentNrs"),
-    ShareOutlinedIcon,
-  ],
-  [
-    "emailQueryLink",
-    () => StandinFilter("showAndPositionSegmentNrs"),
-    ForwardToInboxIcon,
-  ],
+  ["download", onDownload, FileDownloadIcon],
+  ["copyQueryTitle", onCopyQueryTitle, LocalOfferOutlinedIcon],
+  ["copyQueryLink", onCopyQueryLink, ShareOutlinedIcon],
+  ["emailQueryLink", onEmailQueryLink, ForwardToInboxIcon],
 ];
 
 type Props = {
   children: React.ReactNode;
 };
 
-const DisplayOptionsList: React.FC<Props> = ({ children }) => {
+const UtilityOptionsList: React.FC<Props> = ({ children }) => {
   const { t } = useTranslation("settings");
 
   if (isOnlyNull(children as (React.ReactNode | null)[])) {
@@ -73,14 +125,19 @@ const DisplayOptionsList: React.FC<Props> = ({ children }) => {
 
 export const UtilityOptions = () => {
   const currentDbView = useAtomValue(currentDbViewAtom);
-  const { sourceLanguage } = useDbQueryParams();
+  const { fileName, sourceLanguage } = useDbQueryParams();
   const { t } = useTranslation("settings");
+  const [snackbarIsOpen, setSnackbarIsOpen] = useState({
+    download: false,
+    copyQueryTitle: false,
+    copyQueryLink: false,
+    emailQueryLink: false,
+  });
 
   return (
-    <DisplayOptionsList>
+    <UtilityOptionsList>
       {utilityOptionComponents.map((option) => {
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        const [name, UtilityOptionComponent, Icon] = option;
+        const [name, handleClick, Icon] = option;
 
         if (
           isSettingOmitted({
@@ -95,15 +152,28 @@ export const UtilityOptions = () => {
 
         return (
           <ListItem key={name} disablePadding>
-            <ListItemButton>
+            <ListItemButton
+              onClick={() =>
+                handleClick(fileName, [snackbarIsOpen, setSnackbarIsOpen])
+              }
+            >
               <ListItemIcon>
                 <Icon />
               </ListItemIcon>
               <ListItemText primary={t(`optionsLabels.${name}`)} />
             </ListItemButton>
+            <Snackbar
+              message={t(`optionsSnackbarMsgs.${name}`)}
+              anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+              autoHideDuration={3000}
+              open={snackbarIsOpen[name]}
+              onClose={() =>
+                setSnackbarIsOpen({ ...snackbarIsOpen, [name]: false })
+              }
+            />
           </ListItem>
         );
       })}
-    </DisplayOptionsList>
+    </UtilityOptionsList>
   );
 };
