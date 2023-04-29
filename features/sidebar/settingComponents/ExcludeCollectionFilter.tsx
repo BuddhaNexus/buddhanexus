b960c03cfc43efd42438f7840643d37bc084dad9
aminahbl <aminahbl.github@37e.uk>
2023-04-29 14:32:30 +0100

    add base for include/exclude filters
    awaits backend updates for completion

--- features/sidebar/settingComponents/ExcludeCollectionFilter.tsx
+++ features/sidebar/settingComponents/ExcludeCollectionFilter.tsx
@@ -1,287 +1,52 @@
-// /**
-//  * https://mui.com/material-ui/react-autocomplete/
-//  * https://codesandbox.io/s/2326jk?file=/demo.tsx
-//  */
-
-// import React from "react";
-// import { type ListChildComponentProps, VariableSizeList } from "react-window";
-// import { useTranslation } from "next-i18next";
-// import { useDbQueryParams } from "@components/hooks/useDbQueryParams";
-// import { useTextLists } from "@components/hooks/useTextLists";
+import React, { useEffect, useState } from "react";
+import { useTranslation } from "next-i18next";
 import {
-  //   Autocomplete,
-  //   autocompleteClasses,
-  Box,
-  //   CircularProgress,
-  //   FormLabel,
-  //   ListSubheader,
-  //   Popper,
-  //   TextField,
-  //   Typography,
-} from "@mui/material";
-// import { styled } from "@mui/styles";
-// import { atom, useAtom } from "jotai";
-// import type { CategoryMenuItem, TextMenuItem } from "utils/api/textLists";
-// import type { QueryValues } from "features/sidebar/common/dbSidebarSettings";
-// import {
-//   type CoercedCollectionValues,
-//   limitCollectionFilterValueAtom,
-// } from "features/sidebar/common/dbSidebarSettings";
-
-// const OuterElementContext = React.createContext({});
-
-// // eslint-disable-next-line react/display-name
-// const OuterElementType = React.forwardRef<HTMLDivElement>((props, ref) => {
-//   const outerProps = React.useContext(OuterElementContext);
-//   return <div ref={ref} {...props} {...outerProps} />;
-// });
-
-// function useResetCache(data: any) {
-//   const ref = React.useRef<VariableSizeList>(null);
-//   React.useEffect(() => {
-//     // eslint-disable-next-line eqeqeq,no-eq-null
-//     if (ref.current != null) {
-//       ref.current.resetAfterIndex(0, true);
-//     }
-//   }, [data]);
-//   return ref;
-// }
-
-// // px
-// const LISTBOX_PADDING = 8;
-
-// const Row = (props: ListChildComponentProps) => {
-//   const { data, index, style } = props;
-//   const dataSet = data[index];
-//   const inlineStyle = {
-//     ...style,
-//     top: (style.top as number) + LISTBOX_PADDING,
-//     fontWeight: 700,
-//   };
-
-//   // eslint-disable-next-line no-prototype-builtins
-//   if (dataSet.hasOwnProperty("group")) {
-//     return (
-//       <ListSubheader key={dataSet.key} component="div" style={inlineStyle}>
-//         {dataSet.group}
-//       </ListSubheader>
-//     );
-//   }
-
-//   const [dataSetProps, { name, ref }] = dataSet;
-
-//   return (
-//     <Box
-//       {...dataSetProps}
-//       style={inlineStyle}
-//       sx={{
-//         display: "flex",
-//         justifyContent: "space-between",
-//         flex: 1,
-//         "&:nth-of-type(even)": {
-//           bgcolor: "background.accent",
-//         },
-//         "&:hover": { textDecoration: "underline" },
-//       }}
-//       component="li"
-//     >
-//       <div
-//         style={{
-//           overflow: "hidden",
-//           textOverflow: "ellipsis",
-//           padding: "4px 0",
-//         }}
-//       >
-//         <Typography
-//           sx={{
-//             display: "inline",
-//             whiteSpace: "normal",
-//             wordBreak: "break-all",
-//           }}
-//         >
-//           <Typography
-//             component="span"
-//             variant="subtitle2"
-//             sx={{
-//               textTransform: "uppercase",
-//               fontWeight: 600,
-//               pr: 1,
-//             }}
-//           >
-//             {ref}
-//           </Typography>
-//           {name.replace(/^•\s/g, "")}
-//         </Typography>
-//       </div>
-//     </Box>
-//   );
-// };
-
-// // Adapter for react-window
-// const ListboxComponent = React.forwardRef<
-//   HTMLDivElement,
-//   React.HTMLAttributes<HTMLElement>
-// >(function ListboxComponent(props, ref) {
-//   const { children, ...other } = props;
-//   const itemData: React.ReactChild[] = [];
-//   // eslint-disable-next-line unicorn/no-array-for-each
-//   (children as React.ReactChild[]).forEach(
-//     (item: React.ReactChild & { children?: React.ReactChild[] }) => {
-//       itemData.push(item, ...(item.children ?? []));
-//     }
-//   );
-
-//   const itemCount = itemData.length;
-//   const itemSize = 56;
-
-//   const getChildSize = (child: React.ReactChild) => {
-//     // eslint-disable-next-line no-prototype-builtins
-//     if (child.hasOwnProperty("group")) {
-//       return 48;
-//     }
-
-//     const charsInLine = 58;
-//     // @ts-expect-error type issue
-//     const lineCount = Math.ceil(child[1].name.length / charsInLine);
-//     return itemSize * lineCount;
-//   };
-
-//   const getHeight = () => {
-//     if (itemCount > 8) {
-//       return 10 * itemSize;
-//     }
-//     return itemData.map(getChildSize).reduce((a, b) => a + b, 0);
-//   };
-
-//   const gridRef = useResetCache(itemCount);
-
-//   return (
-//     <div ref={ref}>
-//       <OuterElementContext.Provider value={other}>
-//         <VariableSizeList
-//           ref={gridRef}
-//           itemData={itemData}
-//           height={getHeight() + 2 * LISTBOX_PADDING}
-//           width="100%"
-//           outerElementType={OuterElementType}
-//           innerElementType="ul"
-//           itemSize={(index: number) => getChildSize(itemData[index])}
-//           overscanCount={5}
-//           itemCount={itemCount}
-//         >
-//           {Row}
-//         </VariableSizeList>
-//       </OuterElementContext.Provider>
-//     </div>
-//   );
-// });
-
-// const StyledPopper = styled(Popper)({
-//   [`& .${autocompleteClasses.listbox}`]: {
-//     boxSizing: "border-box",
-//     "& ul": {
-//       padding: 0,
-//       margin: 0,
-//     },
-//   },
-// });
-
-// export const DEFAULT_DISABLE_LIMIT_SELECT_STATE = {
-//   excludedCategories: false,
-//   excludedTexts: false,
-//   includedCategories: false,
-//   includedTexts: false,
-// };
-
-// export const disableLimitColectionSelectAtom = atom(
-//   DEFAULT_DISABLE_LIMIT_SELECT_STATE
-// );
+  ListboxComponent,
+  StyledPopper,
+} from "@components/common/textMenuSubComponents";
+import { useTextLists } from "@components/hooks/useTextLists";
+import { Autocomplete, Box, CircularProgress, TextField } from "@mui/material";
+import { DEFAULT_QUERY_PARAMS } from "features/sidebar/common/dbSidebarSettings";
+import { ArrayParam, useQueryParam } from "use-query-params";
+import type { CategoryMenuItem } from "utils/api/textLists";
 
 const ExcludeCollectionFilter = () => {
-  //   // TODO: option filter on select to remove inaplicable texts
+  const { t } = useTranslation("settings");
 
-  //   const { t } = useTranslation("settings");
-  //   const { setQueryParams } = useDbQueryParams();
-  //   const { texts, categories, isLoadingCategories, isLoadingTexts } =
-  //     useTextLists();
+  const { categories, isLoadingCategories } = useTextLists();
 
-  //   const [limitCollectionValues, setLimitCollectionValues] = useAtom(
-  //     limitCollectionFilterValueAtom
-  //   );
+  const [excludeCollectonParam, setExcludeCollectonParam] = useQueryParam(
+    // TODO: replace with "exclude_collection",
+    "limit_collection",
+    ArrayParam
+  );
 
-  //   const [disableSelectors, setDisableSelectors] = useAtom(
-  //     disableLimitColectionSelectAtom
-  //   );
+  const [excludeCollectonValue, setExcludeCollectonValue] = useState<
+    CategoryMenuItem[]
+  >([]);
 
-  //   function setIsSelectorDisabled(
-  //     key: keyof QueryValues["limit_collection"],
-  //     value: boolean
-  //   ) {
-  //     setDisableSelectors((prevState) => {
-  //       const updates = {
-  //         excludedCategories: {},
-  //         excludedTexts: {},
-  //         includedCategories: {
-  //           excludedCategories: !value,
-  //           excludedTexts: !value,
-  //         },
-  //         includedTexts: {
-  //           excludedCategories: !value,
-  //           excludedTexts: !value,
-  //           includedCategories: !value,
-  //         },
-  //       };
-  //       return { ...prevState, ...updates[key] };
-  //     });
-  //   }
+  useEffect(
+    () =>
+      setExcludeCollectonParam(
+        excludeCollectonParam ?? DEFAULT_QUERY_PARAMS.exclude_collection
+      ),
+    [excludeCollectonParam, setExcludeCollectonParam]
+  );
 
-  //   const handleInputChange = (
-  //     newValues: (CategoryMenuItem | TextMenuItem)[],
-  //     filterType: keyof QueryValues["limit_collection"]
-  //   ) => {
-  //     const updatedSelectorValues = new Map();
-
-  //     for (const value of newValues) {
-  //       if (filterType.includes("included")) {
-  //         limitCollectionValues.excludedCategories.clear();
-  //         limitCollectionValues.excludedTexts.clear();
-  //       }
-
-  //       if (filterType === "includedTexts") {
-  //         limitCollectionValues.includedCategories.clear();
-  //       }
-
-  //       updatedSelectorValues.set(value.id, value);
-  //     }
-
-  //     const updatedFilterValues = {
-  //       ...limitCollectionValues,
-  //       [filterType]: updatedSelectorValues,
-  //     };
-
-  //     setLimitCollectionValues(updatedFilterValues);
-
-  //     const updatedParams = Object.keys(updatedFilterValues).flatMap((key) => {
-  //       return [...updatedFilterValues[key as keyof CoercedCollectionValues]].map(
-  //         ([, value]) => (key.includes("excluded") ? `!${value.id}` : value.id)
-  //       );
-  //     });
-
-  //     setQueryParams({
-  //       limit_collection: updatedParams.length > 0 ? updatedParams : undefined,
-  //     });
-
-  //     setIsSelectorDisabled(filterType, newValues.length === 0);
-  //   };
+  const handleInputChange = (value: CategoryMenuItem[]) => {
+    setExcludeCollectonValue(value);
+    setExcludeCollectonParam(() => {
+      return value.map((item) => `!${item.id}`);
+    });
+  };
 
   return (
     <Box sx={{ my: 1, width: 1 }}>
-      {/*  
       <Autocomplete
         id="excluded-collections"
         sx={{ mt: 1, mb: 2 }}
         multiple={true}
-        value={[...limitCollectionValues.excludedCategories.values()]}
+        value={excludeCollectonValue ?? []}
         PopperComponent={StyledPopper}
         ListboxComponent={ListboxComponent}
         options={[...categories.values()]}
@@ -306,14 +71,10 @@ const ExcludeCollectionFilter = () => {
         renderOption={(props, option) => [props, option] as React.ReactNode}
         renderGroup={(params) => params as unknown as React.ReactNode}
         loading={isLoadingCategories}
-        disabled={disableSelectors.excludedCategories}
         filterSelectedOptions
         disablePortal
-        onChange={(event, value) =>
-          handleInputChange(value, "excludedCategories")
-        }
+        onChange={(event, value) => handleInputChange(value)}
       />
-      */}
     </Box>
   );
 };
--- features/sidebar/settingComponents/ExcludeTextFilter.tsx
+++ features/sidebar/settingComponents/ExcludeTextFilter.tsx
@@ -1,292 +1,54 @@
-// /**
-//  * https://mui.com/material-ui/react-autocomplete/
-//  * https://codesandbox.io/s/2326jk?file=/demo.tsx
-//  */
-
-// import React from "react";
-// import { type ListChildComponentProps, VariableSizeList } from "react-window";
-// import { useTranslation } from "next-i18next";
-// import { useDbQueryParams } from "@components/hooks/useDbQueryParams";
-// import { useTextLists } from "@components/hooks/useTextLists";
+import React, { useEffect, useState } from "react";
+import { useTranslation } from "next-i18next";
 import {
-  //   Autocomplete,
-  //   autocompleteClasses,
-  Box,
-  //   CircularProgress,
-  //   FormLabel,
-  //   ListSubheader,
-  //   Popper,
-  //   TextField,
-  //   Typography,
-} from "@mui/material";
-// import { styled } from "@mui/styles";
-// import { atom, useAtom } from "jotai";
-// import type { CategoryMenuItem, TextMenuItem } from "utils/api/textLists";
-// import type { QueryValues } from "features/sidebar/common/dbSidebarSettings";
-// import {
-//   type CoercedCollectionValues,
-//   limitCollectionFilterValueAtom,
-// } from "features/sidebar/common/dbSidebarSettings";
-
-// const OuterElementContext = React.createContext({});
-
-// // eslint-disable-next-line react/display-name
-// const OuterElementType = React.forwardRef<HTMLDivElement>((props, ref) => {
-//   const outerProps = React.useContext(OuterElementContext);
-//   return <div ref={ref} {...props} {...outerProps} />;
-// });
-
-// function useResetCache(data: any) {
-//   const ref = React.useRef<VariableSizeList>(null);
-//   React.useEffect(() => {
-//     // eslint-disable-next-line eqeqeq,no-eq-null
-//     if (ref.current != null) {
-//       ref.current.resetAfterIndex(0, true);
-//     }
-//   }, [data]);
-//   return ref;
-// }
-
-// // px
-// const LISTBOX_PADDING = 8;
-
-// const Row = (props: ListChildComponentProps) => {
-//   const { data, index, style } = props;
-//   const dataSet = data[index];
-//   const inlineStyle = {
-//     ...style,
-//     top: (style.top as number) + LISTBOX_PADDING,
-//     fontWeight: 700,
-//   };
-
-//   // eslint-disable-next-line no-prototype-builtins
-//   if (dataSet.hasOwnProperty("group")) {
-//     return (
-//       <ListSubheader key={dataSet.key} component="div" style={inlineStyle}>
-//         {dataSet.group}
-//       </ListSubheader>
-//     );
-//   }
-
-//   const [dataSetProps, { name, ref }] = dataSet;
-
-//   return (
-//     <Box
-//       {...dataSetProps}
-//       style={inlineStyle}
-//       sx={{
-//         display: "flex",
-//         justifyContent: "space-between",
-//         flex: 1,
-//         "&:nth-of-type(even)": {
-//           bgcolor: "background.accent",
-//         },
-//         "&:hover": { textDecoration: "underline" },
-//       }}
-//       component="li"
-//     >
-//       <div
-//         style={{
-//           overflow: "hidden",
-//           textOverflow: "ellipsis",
-//           padding: "4px 0",
-//         }}
-//       >
-//         <Typography
-//           sx={{
-//             display: "inline",
-//             whiteSpace: "normal",
-//             wordBreak: "break-all",
-//           }}
-//         >
-//           <Typography
-//             component="span"
-//             variant="subtitle2"
-//             sx={{
-//               textTransform: "uppercase",
-//               fontWeight: 600,
-//               pr: 1,
-//             }}
-//           >
-//             {ref}
-//           </Typography>
-//           {name.replace(/^•\s/g, "")}
-//         </Typography>
-//       </div>
-//     </Box>
-//   );
-// };
-
-// // Adapter for react-window
-// const ListboxComponent = React.forwardRef<
-//   HTMLDivElement,
-//   React.HTMLAttributes<HTMLElement>
-// >(function ListboxComponent(props, ref) {
-//   const { children, ...other } = props;
-//   const itemData: React.ReactChild[] = [];
-//   // eslint-disable-next-line unicorn/no-array-for-each
-//   (children as React.ReactChild[]).forEach(
-//     (item: React.ReactChild & { children?: React.ReactChild[] }) => {
-//       itemData.push(item, ...(item.children ?? []));
-//     }
-//   );
-
-//   const itemCount = itemData.length;
-//   const itemSize = 56;
-
-//   const getChildSize = (child: React.ReactChild) => {
-//     // eslint-disable-next-line no-prototype-builtins
-//     if (child.hasOwnProperty("group")) {
-//       return 48;
-//     }
-
-//     const charsInLine = 58;
-//     // @ts-expect-error type issue
-//     const lineCount = Math.ceil(child[1].name.length / charsInLine);
-//     return itemSize * lineCount;
-//   };
-
-//   const getHeight = () => {
-//     if (itemCount > 8) {
-//       return 10 * itemSize;
-//     }
-//     return itemData.map(getChildSize).reduce((a, b) => a + b, 0);
-//   };
-
-//   const gridRef = useResetCache(itemCount);
-
-//   return (
-//     <div ref={ref}>
-//       <OuterElementContext.Provider value={other}>
-//         <VariableSizeList
-//           ref={gridRef}
-//           itemData={itemData}
-//           height={getHeight() + 2 * LISTBOX_PADDING}
-//           width="100%"
-//           outerElementType={OuterElementType}
-//           innerElementType="ul"
-//           itemSize={(index: number) => getChildSize(itemData[index])}
-//           overscanCount={5}
-//           itemCount={itemCount}
-//         >
-//           {Row}
-//         </VariableSizeList>
-//       </OuterElementContext.Provider>
-//     </div>
-//   );
-// });
-
-// const StyledPopper = styled(Popper)({
-//   [`& .${autocompleteClasses.listbox}`]: {
-//     boxSizing: "border-box",
-//     "& ul": {
-//       padding: 0,
-//       margin: 0,
-//     },
-//   },
-// });
-
-// export const DEFAULT_DISABLE_LIMIT_SELECT_STATE = {
-//   excludedCategories: false,
-//   excludedTexts: false,
-//   includedCategories: false,
-//   includedTexts: false,
-// };
-
-// export const disableLimitColectionSelectAtom = atom(
-//   DEFAULT_DISABLE_LIMIT_SELECT_STATE
-// );
+  ListboxComponent,
+  StyledPopper,
+} from "@components/common/textMenuSubComponents";
+import { useTextLists } from "@components/hooks/useTextLists";
+import { Autocomplete, Box, CircularProgress, TextField } from "@mui/material";
+import { DEFAULT_QUERY_PARAMS } from "features/sidebar/common/dbSidebarSettings";
+import { ArrayParam, useQueryParam } from "use-query-params";
+import type { TextMenuItem } from "utils/api/textLists";
 
 const ExcludeTextFilter = () => {
-  //   // TODO: option filter on select to remove inaplicable texts
+  const { t } = useTranslation("settings");
 
-  //   const { t } = useTranslation("settings");
-  //   const { setQueryParams } = useDbQueryParams();
-  //   const { texts, categories, isLoadingCategories, isLoadingTexts } =
-  //     useTextLists();
+  const { texts, isLoadingTexts } = useTextLists();
 
-  //   const [limitCollectionValues, setLimitCollectionValues] = useAtom(
-  //     limitCollectionFilterValueAtom
-  //   );
+  const [excludeTextParam, setExcludeTextParam] = useQueryParam(
+    // TODO: replace with "exclude_text",
+    "limit_collection",
+    ArrayParam
+  );
 
-  //   const [disableSelectors, setDisableSelectors] = useAtom(
-  //     disableLimitColectionSelectAtom
-  //   );
+  const [excludeTextValue, setExcludeTextValue] = useState<TextMenuItem[]>([]);
 
-  //   function setIsSelectorDisabled(
-  //     key: keyof QueryValues["limit_collection"],
-  //     value: boolean
-  //   ) {
-  //     setDisableSelectors((prevState) => {
-  //       const updates = {
-  //         excludedCategories: {},
-  //         excludedTexts: {},
-  //         includedCategories: {
-  //           excludedCategories: !value,
-  //           excludedTexts: !value,
-  //         },
-  //         includedTexts: {
-  //           excludedCategories: !value,
-  //           excludedTexts: !value,
-  //           includedCategories: !value,
-  //         },
-  //       };
-  //       return { ...prevState, ...updates[key] };
-  //     });
-  //   }
+  useEffect(
+    () =>
+      setExcludeTextParam(
+        excludeTextParam ?? DEFAULT_QUERY_PARAMS.exclude_text
+      ),
+    [excludeTextParam, setExcludeTextParam]
+  );
 
-  //   const handleInputChange = (
-  //     newValues: (CategoryMenuItem | TextMenuItem)[],
-  //     filterType: keyof QueryValues["limit_collection"]
-  //   ) => {
-  //     const updatedSelectorValues = new Map();
-
-  //     for (const value of newValues) {
-  //       if (filterType.includes("included")) {
-  //         limitCollectionValues.excludedCategories.clear();
-  //         limitCollectionValues.excludedTexts.clear();
-  //       }
-
-  //       if (filterType === "includedTexts") {
-  //         limitCollectionValues.includedCategories.clear();
-  //       }
-
-  //       updatedSelectorValues.set(value.id, value);
-  //     }
-
-  //     const updatedFilterValues = {
-  //       ...limitCollectionValues,
-  //       [filterType]: updatedSelectorValues,
-  //     };
-
-  //     setLimitCollectionValues(updatedFilterValues);
-
-  //     const updatedParams = Object.keys(updatedFilterValues).flatMap((key) => {
-  //       return [...updatedFilterValues[key as keyof CoercedCollectionValues]].map(
-  //         ([, value]) => (key.includes("excluded") ? `!${value.id}` : value.id)
-  //       );
-  //     });
-
-  //     setQueryParams({
-  //       limit_collection: updatedParams.length > 0 ? updatedParams : undefined,
-  //     });
-
-  //     setIsSelectorDisabled(filterType, newValues.length === 0);
-  //   };
+  const handleInputChange = (value: TextMenuItem[]) => {
+    setExcludeTextValue(value);
+    setExcludeTextParam(() => {
+      return value.map((item) => `!${item.id}`);
+    });
+  };
 
   return (
     <Box sx={{ my: 1, width: 1 }}>
-      {/*  
       <Autocomplete
         id="excluded-texts"
-        sx={{ mb: 2 }}
+        sx={{ mt: 1, mb: 2 }}
         multiple={true}
-        value={[...limitCollectionValues.excludedTexts.values()]}
+        value={excludeTextValue ?? []}
         PopperComponent={StyledPopper}
         ListboxComponent={ListboxComponent}
         options={[...texts.values()]}
         getOptionLabel={(option) => option.name.toUpperCase()}
-        groupBy={(option) => option.category.toUpperCase()}
         renderInput={(params) => (
           <TextField
             {...params}
@@ -307,11 +69,10 @@ const ExcludeTextFilter = () => {
         renderOption={(props, option) => [props, option] as React.ReactNode}
         renderGroup={(params) => params as unknown as React.ReactNode}
         loading={isLoadingTexts}
-        disabled={disableSelectors.excludedTexts}
         filterSelectedOptions
         disablePortal
-        onChange={(event, value) => handleInputChange(value, "excludedTexts")}
-      />*/}
+        onChange={(event, value) => handleInputChange(value)}
+      />
     </Box>
   );
 };
