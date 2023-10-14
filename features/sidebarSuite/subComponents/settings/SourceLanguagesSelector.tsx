b62ac54a989ca542c5a13ace01a8eb1a736460d8
aminahbl <aminahbl.github@37e.uk>
2023-10-14 15:15:18 +0200

    fix selector server mismatch

--- features/sidebarSuite/subComponents/settings/SourceLanguagesSelector.tsx
+++ features/sidebarSuite/subComponents/settings/SourceLanguagesSelector.tsx
@@ -3,7 +3,6 @@ import { useTranslation } from "next-i18next";
 import { useDbQueryParams } from "@components/hooks/useDbQueryParams";
 import {
   Box,
-  CircularProgress,
   FormControl,
   InputLabel,
   ListItemText,
@@ -62,7 +61,9 @@ const SourceLanguagesSelector = () => {
 
   /* eslint-disable react-hooks/exhaustive-deps */
   React.useEffect(() => {
-    setSelectedLanguages(selectedLanguages ?? data);
+    if (!selectedLanguages) {
+      setSelectedLanguages(data);
+    }
   }, [setSelectedLanguages, isLoading]);
 
   const handleChange = (event: SelectChangeEvent<typeof selectedLanguages>) => {
@@ -76,79 +77,84 @@ const SourceLanguagesSelector = () => {
     );
   };
 
+  const selectorLabel = t("optionsLabels.availableLanguages");
+
   return (
     <Box sx={{ width: 1, my: 2 }}>
       <FormControl sx={{ width: 1 }}>
         <InputLabel id="text-languages-selector-label">
-          {t("optionsLabels.availableLanguages")}
+          {selectorLabel}
         </InputLabel>
-        {isLoading ? (
-          <Select
-            labelId="text-languages-label"
-            id="text-languages"
-            value={selectedLanguages ?? []}
-            input={
-              <OutlinedInput label={t("optionsLabels.availableLanguages")} />
-            }
-            MenuProps={MenuProps}
-            multiple
-            onChange={handleChange}
-          >
-            <MenuItem value="loading">
-              <CircularProgress color="inherit" size={20} />
-            </MenuItem>
-          </Select>
-        ) : (
-          <Select
-            labelId="text-languages-label"
-            id="text-languages"
-            value={selectedLanguages ?? []}
-            input={
-              <OutlinedInput label={t("optionsLabels.availableLanguages")} />
-            }
-            MenuProps={MenuProps}
-            renderValue={(selected) => {
-              const renderedSelection = selected
-                .map((selection) =>
-                  t(
-                    `dbLanguageLabels.${selection as unknown as SourceLanguage}`
+        <Select
+          labelId="text-languages-selector-label"
+          id="text-languages"
+          value={isLoading ? ["loading"] : [...(selectedLanguages ?? [])]}
+          input={<OutlinedInput label={selectorLabel} />}
+          MenuProps={MenuProps}
+          renderValue={(selected) => {
+            const renderedSelection = selected
+              ? selected
+                  .map((selection) =>
+                    t(
+                      `dbLanguageLabels.${
+                        selection as unknown as SourceLanguage
+                      }`
+                    )
                   )
-                )
-                .join(", ");
+                  .join(", ")
+              : "";
 
-              return renderedSelection;
-            }}
-            multiple
-            displayEmpty
-            onChange={handleChange}
-          >
-            {data?.map((lang: SourceLanguage) => {
-              return (
-                <MenuItem
-                  key={lang}
-                  value={lang}
-                  style={getStyles(
-                    lang,
-                    selectedLanguages as SourceLanguage[],
-                    theme
-                  )}
-                >
-                  <Checkbox
-                    checked={
-                      selectedLanguages
-                        ? selectedLanguages.includes(lang)
-                        : false
-                    }
-                  />
-                  <ListItemText primary={t(`dbLanguageLabels.${lang}`)} />
-                </MenuItem>
-              );
-            })}
-          </Select>
-        )}
+            return renderedSelection;
+          }}
+          multiple
+          displayEmpty
+          onChange={handleChange}
+        >
+          {data?.map((lang: SourceLanguage) => {
+            return (
+              <MenuItem
+                key={lang}
+                value={lang}
+                style={getStyles(
+                  lang,
+                  selectedLanguages as SourceLanguage[],
+                  theme
+                )}
+              >
+                <Checkbox
+                  checked={
+                    selectedLanguages ? selectedLanguages.includes(lang) : false
+                  }
+                />
+                <ListItemText primary={t(`dbLanguageLabels.${lang}`)} />
+              </MenuItem>
+            );
+          })}
+        </Select>
       </FormControl>
     </Box>
   );
 };
 
 export default SourceLanguagesSelector;
+
+// {isLoading ? (
+//   <Select
+//     labelId="text-languages-selector-label"
+//     id="text-languages"
+//     value={selectedLanguages ?? []}
+//     input={
+//       <OutlinedInput label={selectorLabel} />
+//     }
+//     MenuProps={MenuProps}
+//     multiple
+//     displayEmpty
+//     onChange={handleChange}
+//   >
+//     <MenuItem value="loading">
+//       <CircularProgress color="inherit" size={20} />
+//     </MenuItem>
+//   </Select>
+// ) : (
+
+// )}
