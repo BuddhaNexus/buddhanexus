c8daccaf8bccc63ff792785d77b9aa8d02289afc
Hubert Dworczyński <7022085+TrebuhD@users.noreply.github.com>
2023-11-04 21:41:46 +0100

    disable prefetching of all data

--- pages/db/[language]/index.tsx
+++ pages/db/[language]/index.tsx
@@ -6,11 +6,11 @@ import { useDbQueryParams } from "@components/hooks/useDbQueryParams";
 import { Footer } from "@components/layout/Footer";
 import { PageContainer } from "@components/layout/PageContainer";
 import { Paper, Typography } from "@mui/material";
-import { dehydrate } from "@tanstack/react-query";
+// import { dehydrate } from "@tanstack/react-query";
 import { SourceTextBrowserDrawer } from "features/sourceTextBrowserDrawer/sourceTextBrowserDrawer";
 import merge from "lodash/merge";
-import { prefetchDefaultDbPageData } from "utils/api/apiQueryUtils";
-import type { SourceLanguage } from "utils/constants";
+// import { prefetchDefaultDbPageData } from "utils/api/apiQueryUtils";
+// import type { SourceLanguage } from "utils/constants";
 import { getI18NextStaticProps } from "utils/nextJsHelpers";
 
 export { getSourceLanguageStaticPaths as getStaticPaths } from "utils/nextJsHelpers";
@@ -34,7 +34,10 @@ export default function DbIndexPage() {
   );
 }
 
-export const getStaticProps: GetStaticProps = async ({ locale, params }) => {
+export const getStaticProps: GetStaticProps = async ({
+  locale,
+  // params
+}) => {
   const i18nProps = await getI18NextStaticProps(
     {
       locale,
@@ -42,12 +45,12 @@ export const getStaticProps: GetStaticProps = async ({ locale, params }) => {
     ["db", "settings"],
   );
 
-  const queryClient = await prefetchDefaultDbPageData(
-    params?.language as SourceLanguage,
-  );
+  // const queryClient = await prefetchDefaultDbPageData(
+  //   params?.language as SourceLanguage,
+  // );
 
   return merge(
-    { props: { dehydratedState: dehydrate(queryClient) } },
+    // { props: { dehydratedState: dehydrate(queryClient) } },
     i18nProps,
   );
 };
