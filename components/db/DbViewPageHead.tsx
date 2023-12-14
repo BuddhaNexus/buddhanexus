37edbb7985097f216ef90c28badf66466cd6b2c3
aminahbl <aminahbl.github@37e.uk>
2023-12-14 20:52:44 +0100

    add page title for db results pages

--- components/db/DbViewPageHead.tsx
+++ components/db/DbViewPageHead.tsx
@@ -1,20 +1,31 @@
+import { NextSeo } from "next-seo";
 import { QueryPageTopStack } from "@components/db/QueryPageTopStack";
 import { useDbQueryParams } from "@components/hooks/useDbQueryParams";
+import { currentViewAtom } from "@components/hooks/useDbView";
 import { Typography } from "@mui/material";
 import { useQuery } from "@tanstack/react-query";
+import { useAtomValue } from "jotai";
+import { startCase } from "lodash";
 import { OldDbApi } from "utils/api/dbApi";
 
 export const DbViewPageHead = () => {
-  // TODO: get full text name
   const { fileName } = useDbQueryParams();
 
+  // TODO: the current implementation uses an endpoint from the old API, we should switch to the new one when available
   const { data: displayName, isLoading } = useQuery({
     queryKey: OldDbApi.TextDisplayName.makeQueryKey(fileName),
     queryFn: () => OldDbApi.TextDisplayName.call(fileName),
   });
 
+  const dbView = useAtomValue(currentViewAtom);
+
   return (
     <>
+      <NextSeo
+        title={`BuddhaNexus — ${fileName} :: ${displayName} :: ${startCase(
+          dbView,
+        )} View`}
+      />
       <QueryPageTopStack />
       {isLoading ? (
         <Typography variant="h2" component="h1" mb={1}>
