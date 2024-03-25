a2981608c5f2295256f1f3a6e3b7578eaddb3fc6
Hubert Dworczyński <hubert.dworczynski@gmail.com>
2024-03-25 10:17:11 +0100

    fix parallel segment header display

--- features/tableView/ParallelSegment.tsx
+++ features/tableView/ParallelSegment.tsx
@@ -70,16 +70,13 @@ export const ParallelSegment = ({
           <SourceLanguageChip label={sourceLanguageName} language={language} />
 
           {/* File Name */}
-          <Tooltip
-            title={textSegmentNumbers}
-            PopperProps={{ disablePortal: true }}
-          >
+          <Tooltip title={displayName} PopperProps={{ disablePortal: true }}>
             <Link
               href={`/db/${language}/${textName}/text?selectedSegment=${urlEncodedSegmentNumber}&selectedSegmentIndex=0`}
               sx={{ display: "inline-block", wordBreak: "break-word", m: 0.5 }}
               target="_blank"
             >
-              {displayName}
+              {textSegmentNumbers}
             </Link>
           </Tooltip>
           <IconButton
