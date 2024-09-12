78ae61ebdf93c0d24e3d9584cacd8e3d24912ed6
aminahbl <aminahbl.github@37e.uk>
2024-09-12 21:16:11 +0100

    #173 - apply atomWithStorage to tib script value

--- features/atoms/index.ts
+++ features/atoms/index.ts
@@ -1,9 +1,13 @@
 import type { Script } from "features/sidebarSuite/subComponents/settings/TextScriptOption";
 import { atom } from "jotai";
+import { atomWithStorage } from "jotai/utils";
 import type { SourceLanguage } from "utils/constants";
 
 export const isNavigationDrawerOpen = atom(false);
-export const scriptSelectionAtom = atom<Script>("Unicode");
+export const scriptSelectionAtom = atomWithStorage<Script>(
+  "text-script-selection",
+  "Unicode",
+);
 export const isSettingsOpenAtom = atom(true);
 export const defaultSourceLanguagesSelection = atom<SourceLanguage[]>([]);
 export const textViewFilterComparisonAtom = atom<string | undefined>(undefined);
