4df5deb6dfe5866cc4e0e71eea58dce0e763e8f5
aminahbl <aminahbl.github@37e.uk>
2024-10-20 15:13:09 +0100

    #316 remove use-query-params (replaced with nuqs)

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -5,7 +5,6 @@ import type { AppProps } from "next/app";
 import Head from "next/head";
 import { appWithTranslation } from "next-i18next";
 import nextI18NextConfig from "next-i18next.config";
-import { NextAdapter } from "next-query-params";
 import { DefaultSeo } from "next-seo";
 import SEO from "next-seo.config";
 import { AppTopBar } from "@components/layout/AppTopBar";
@@ -19,8 +18,6 @@ import {
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { queryCacheTimeDefaults } from "@utils/api/apiQueryUtils";
 import { ThemeProvider } from "@utils/ThemeProvider";
-import queryString from "query-string";
-import { QueryParamProvider } from "use-query-params";
 
 function MyApp(props: AppProps) {
   const { Component, pageProps } = props;
@@ -30,39 +27,29 @@ function MyApp(props: AppProps) {
         defaultOptions: {
           queries: { refetchOnWindowFocus: false, ...queryCacheTimeDefaults },
         },
-      }),
+      })
   );
 
   return (
     <AppCacheProvider {...props}>
-      <QueryParamProvider
-        adapter={NextAdapter}
-        options={{
-          searchStringToObject: queryString.parse,
-          objectToSearchString: queryString.stringify,
-          updateType: "replaceIn",
-          enableBatching: true,
-        }}
-      >
-        <QueryClientProvider client={queryClient}>
-          <HydrationBoundary state={pageProps.dehydratedState}>
-            <DefaultSeo {...SEO} />
-            <Head>
-              <meta
-                name="viewport"
-                content="initial-scale=1, width=device-width"
-              />
-            </Head>
+      <QueryClientProvider client={queryClient}>
+        <HydrationBoundary state={pageProps.dehydratedState}>
+          <DefaultSeo {...SEO} />
+          <Head>
+            <meta
+              name="viewport"
+              content="initial-scale=1, width=device-width"
+            />
+          </Head>
 
-            <ThemeProvider>
-              <CssBaseline />
-              <AppTopBar />
-              <Component {...pageProps} />
-            </ThemeProvider>
-          </HydrationBoundary>
-          <ReactQueryDevtools />
-        </QueryClientProvider>
-      </QueryParamProvider>
+          <ThemeProvider>
+            <CssBaseline />
+            <AppTopBar />
+            <Component {...pageProps} />
+          </ThemeProvider>
+        </HydrationBoundary>
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </AppCacheProvider>
   );
 }
