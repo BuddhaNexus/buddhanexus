de390525d7c1437cfd9df9ff2345c9fddc193de2
ayya-vimala <ayya.vimala@gmail.com>
2024-11-01 12:44:20 +0100

    Revert "links update"
    This reverts commit a63c48b78603f4bae0f2a8d10959c84cf3236bd4.

--- api/links.py
+++ api/links.py
@@ -6,6 +6,18 @@ from .utils import get_language_from_filename
 
 def get_links(filename, links_query):
     """get links for each file name"""
+    if len(links_query.result) < 1:
+        return {
+            "bdrc": False,
+            "rkts": False,
+            "gretil": False,
+            "dsbc": False,
+            "cbeta": False,
+            "suttacentral": False,
+            "cbc": False,
+            "vri": False,
+        }
+
     links_query_result = links_query.result[0]
     link1 = links_query_result[0]
     link2 = links_query_result[1]
@@ -20,7 +32,9 @@ def get_links(filename, links_query):
     lang = get_language_from_filename(filename)
 
     # for Tibetan, we serve links to BDRC and RKTS
-    if lang == "bo" and link1:
+    if lang == "bo" and not filename.startswith(
+        "BO_N"
+    ):  # We exclude N files from external linking
         bdrc = link1
         rkts = bdrc.replace(
             "http://purl.bdrc.io/resource/WA0RK", "http://purl.rkts.eu/resource/WKT"
@@ -31,25 +45,26 @@ def get_links(filename, links_query):
         )
 
     # for Sanskrit, we serve links to Gretil and DSBC and SC
-    if lang == "sa" and link1:
+    if lang == "sa":
         if "gretil" in link1:
             gretil = link1
         elif "dsbc" in link1:
             dsbc = link1
-        if link2:
-            sc_link = link2
+        sc_link = link2
 
     # for Chinese, we serve links to CBETA, SC and CBC
     if lang == "zh":
-        if link1:
-            cbeta = link1
-        if link2:
-            sc_link = link2
+        cbeta = (
+            "https://cbetaonline.dila.edu.tw/" + filename + "_001"
+        )  # why did we do this on the frontend:  re.sub(filename, "_[TX]", "n")
+        sc_link = link2
+        cbc_filename = filename[0] + filename[4:]
+        cbc = "https://dazangthings.nz/cbc/text/" + cbc_filename
 
     if lang == "pa":
         vri = link1 or "https://tipitaka.org/romn/"
-        if link2:
-            sc_link = link2
+        if not re.search("^tika|^anya|^atk", filename):
+            sc_link = "https://suttacentral.net/" + filename
 
     return {
         "bdrc": bdrc,
