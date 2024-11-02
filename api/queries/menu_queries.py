64819fbf3d5238c81468a04aedbcc24bb023d4ea
ayya-vimala <ayya.vimala@gmail.com>
2024-11-02 08:48:21 +0100

    Sorting menu data

--- api/queries/menu_queries.py
+++ api/queries/menu_queries.py
@@ -6,6 +6,7 @@ Contains query for the total menudata.
 QUERY_TOTAL_DATA = """
 FOR file IN files
     FILTER file.lang == @lang
+    SORT file.filenr
     LET category_info = FIRST(
         FOR cat IN category_names
         FILTER cat.category == file.category AND cat.lang == @lang
