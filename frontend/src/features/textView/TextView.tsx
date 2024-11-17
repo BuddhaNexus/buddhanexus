9450b7ca077ad86b2a5f5f9e3e3470b8be22c5ce
Hubert Dworczyński <hubert.dworczynski@gmail.com>
2024-11-17 21:55:57 +0100

    WIP: display third pane in text view

--- frontend/src/features/textView/TextView.tsx
+++ frontend/src/features/textView/TextView.tsx
@@ -1,14 +1,22 @@
 import "allotment/dist/style.css";
 
 import React, { useMemo } from "react";
-import { Virtuoso } from "react-virtuoso";
+import { LogLevel, Virtuoso } from "react-virtuoso";
 import { activeSegmentMatchesAtom } from "@atoms";
 import {
   EmptyPlaceholder,
   ListLoadingIndicator,
 } from "@components/db/ListComponents";
 import { useActiveSegmentParam } from "@components/hooks/params";
+import { DEFAULT_PARAM_VALUES } from "@features/SidebarSuite/uiSettings/config";
+import { useTextViewRightPane } from "@features/textView/useTextViewRightPane";
 import { Paper } from "@mui/material";
+import {
+  keepPreviousData,
+  useInfiniteQuery,
+  useQuery,
+} from "@tanstack/react-query";
+import { DbApi } from "@utils/api/dbApi";
 import { ParsedTextViewParallels } from "@utils/api/endpoints/text-view/text-parallels";
 import { Allotment } from "allotment";
 import chroma from "chroma-js";
@@ -49,19 +57,34 @@ export const TextView = ({
       .correctLightness(true);
   }, [data]);
 
-  const hasData = data.length > 0;
   const shouldShowMiddlePane =
     activeSegmentId !== "none" && activeSegmentMatches.length > 0;
 
+  const shouldShowRightPane = true;
+
   // make sure the selected segment is at the top when the page is opened
   const activeSegmentIndexInData = useMemo(() => {
-    if (!hasData) return 0;
+    if (data.length <= 0) return 0;
     const index = data.findIndex(
       (element) => element.segmentNumber === activeSegmentId,
     );
     if (index === -1) return 0;
     return index;
-  }, [data, hasData, activeSegmentId]);
+  }, [data, activeSegmentId]);
+
+  const {
+    data: rightPaneData,
+    isFetchingPreviousPage: isFetchingRightPanePreviousPage,
+    isFetchingNextPage: isFetchingRightPaneNextPage,
+    error: rightPaneError,
+    isSuccess: isRightPaneSuccess,
+    fetchPreviousPage: fetchRightPanePreviousPage,
+    fetchNextPage: fetchRightPaneNextPage,
+    isFetching: isRightPaneFetching,
+    isError: isRightPaneError,
+  } = useTextViewRightPane(shouldShowRightPane);
+
+  console.log({ rightPaneData, data });
 
   return (
     <Paper sx={{ flex: 1, py: 1, pl: 2, my: 1 }}>
@@ -71,7 +94,7 @@ export const TextView = ({
           <Virtuoso
             firstItemIndex={firstItemIndex}
             initialTopMostItemIndex={activeSegmentIndexInData}
-            data={hasData && data.length > 0 ? data : undefined}
+            data={data.length > 0 ? data : undefined}
             startReached={onStartReached}
             endReached={onEndReached}
             totalCount={data.length}
@@ -92,6 +115,41 @@ export const TextView = ({
         {/* Middle pane - parallels for selected segment */}
         <Allotment.Pane visible={shouldShowMiddlePane}>
           <TextViewMiddleParallels />
+        </Allotment.Pane>
+
+        {/* Right Pane - shown after a parallel is selected in middle pane */}
+        {/* There's some initial rendering issue here, not sure why key prop: */}
+        <Allotment.Pane
+          key={String(rightPaneData)}
+          visible={shouldShowRightPane}
+        >
+          {/* TODO: plug different data in here */}
+          <Virtuoso
+            id="2"
+            logLevel={LogLevel.DEBUG}
+            // firstItemIndex={firstItemIndex}
+            // initialTopMostItemIndex={activeSegmentIndexInData}
+            data={rightPaneData.length > 0 ? rightPaneData : undefined}
+            startReached={fetchRightPanePreviousPage}
+            endReached={fetchRightPaneNextPage}
+            totalCount={rightPaneData.length}
+            overscan={900} // pixel value
+            increaseViewportBy={500} // solves empty content at start/end of list issue
+            initialItemCount={5} // for SSR
+            components={{
+              Header: isFetchingRightPanePreviousPage
+                ? ListLoadingIndicator
+                : undefined,
+              Footer: isFetchingRightPaneNextPage
+                ? ListLoadingIndicator
+                : undefined,
+              EmptyPlaceholder,
+            }}
+            itemContent={(_, dataSegment) => {
+              console.log("hmmm");
+              return <TextSegment data={dataSegment} colorScale={colorScale} />;
+            }}
+          />
         </Allotment.Pane>
       </Allotment>
     </Paper>
---
+++ frontend/src/features/textView/useTextViewRightPane.ts
@@ -0,0 +1,135 @@
+import { useCallback, useEffect, useMemo, useRef } from "react";
+import { useSearchParams } from "next/navigation";
+import { useStandardViewBaseQueryParams } from "@components/hooks/groupedQueryParams";
+import { DEFAULT_PARAM_VALUES } from "@features/SidebarSuite/uiSettings/config";
+import { keepPreviousData, useInfiniteQuery } from "@tanstack/react-query";
+import { DbApi } from "@utils/api/dbApi";
+
+export function useTextViewRightPane(shouldShowRightPane: boolean) {
+  const paginationState = useRef<PaginationState>([0, 0]);
+
+  const searchParams = useSearchParams();
+
+  const active_segment_right =
+    searchParams.get("active_segment_right") ??
+    DEFAULT_PARAM_VALUES.active_segment;
+
+  const initialPageParam =
+    active_segment_right === DEFAULT_PARAM_VALUES.active_segment
+      ? 0
+      : undefined;
+
+  const requestBodyBase = useStandardViewBaseQueryParams();
+
+  const {
+    data,
+    isSuccess,
+    fetchNextPage,
+    fetchPreviousPage,
+    isFetchingPreviousPage,
+    isFetchingNextPage,
+    isFetching,
+    isError,
+    error,
+  } = useInfiniteQuery({
+    enabled: shouldShowRightPane,
+    placeholderData: keepPreviousData,
+    initialPageParam,
+    queryKey: DbApi.TextView.makeQueryKey({
+      ...requestBodyBase,
+      active_segment: active_segment_right,
+    }),
+    queryFn: ({ pageParam }) => {
+      // We pass the active_segment, but only on the first page load :/
+      //
+      // This is a bit of a workaround to enable scrolling up. Explanation:
+      // When `active_segment` is inside a query param, the BE always responds with the page that includes the segment.
+      // We pass it to the backend, and we assume the page is 0. In the BE response, it tells us that we're on page 1,
+      // but there's no way to request page 0 when `active_segment` is included.
+      //
+      // A possible issue with this workaround is that it only runs on the client side.
+      // We may need to revisit after moving to the Next.js App Router
+
+      // if the `active_segment` param was already sent for this segment,
+      // don't send it anymore.
+      // const activeSegmentParam = hasSegmentBeenSelected(active_segment)
+      //   ? DEFAULT_PARAM_VALUES.active_segment
+      //   : active_segment;
+
+      return DbApi.TextView.call({
+        ...requestBodyBase,
+        page: pageParam ?? 0,
+        active_segment: active_segment_right,
+      });
+    },
+
+    getPreviousPageParam: () => {
+      // if it's the first page, don't fetch more
+      const [startEdge] = paginationState.current;
+      if (startEdge === undefined || startEdge === 0) return undefined;
+      return startEdge - 1;
+    },
+
+    getNextPageParam: (lastPage) => {
+      const [, endEdge] = paginationState.current;
+      if (endEdge === undefined || endEdge === lastPage.data.totalPages - 1) {
+        // last page, as indicated by the BE response
+        return undefined;
+      }
+      return endEdge + 1;
+    },
+  });
+
+  useEffect(
+    function handleApiResponse() {
+      if (!data?.pages[0]) {
+        return;
+      }
+      const currentPageCount = data?.pages?.length;
+      // when the first page is loaded, set the current page number to the one received from the BE
+      if (currentPageCount === 1) {
+        paginationState.current[0] = data?.pages[0].data.page;
+        paginationState.current[1] = data?.pages[0].data.page;
+      }
+    },
+    [data?.pages],
+  );
+
+  const allParallels = useMemo(
+    () => (data?.pages ? data.pages.flatMap((page) => page.data.items) : []),
+    [data?.pages],
+  );
+
+  const handleFetchingPreviousPage = useCallback(async () => {
+    // already on first page
+    if (paginationState.current[0] === 0) return;
+
+    const { data: responseData } = await fetchPreviousPage();
+    // eslint-disable-next-line require-atomic-updates
+    paginationState.current[0] = responseData?.pages[0]?.data.page;
+
+    const fetchedPageSize = responseData?.pages[0]?.data.items?.length;
+    if (!fetchedPageSize) return;
+
+    // the user is scrolling up.
+    // offset the new list items when prepending them to the page.
+    setFirstItemIndex((prevIndex) => prevIndex - fetchedPageSize);
+  }, [fetchPreviousPage]);
+
+  const handleFetchingNextPage = useCallback(async () => {
+    const response = await fetchNextPage();
+    paginationState.current[1] = response.data?.pages.at(-1)?.data.page;
+  }, [fetchNextPage]);
+
+  return {
+    data: allParallels,
+    isFetchingPreviousPage,
+    isFetchingNextPage,
+    isSuccess,
+    fetchNextPage: handleFetchingNextPage,
+    fetchPreviousPage: handleFetchingPreviousPage,
+    isError,
+    isFetching,
+    error,
+  };
+}
