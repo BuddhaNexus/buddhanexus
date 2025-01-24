1694bdbcc720c4348963ebd9fbf45227228b2060
aminahbl <aminahbl.github@37e.uk>
2025-01-24 13:39:39 +0000

    update sankey nav menu label
    also includes cosmentic home page fixes

--- frontend/src/components/layout/DbLanguageLinkBox.tsx
+++ frontend/src/components/layout/DbLanguageLinkBox.tsx
@@ -3,7 +3,6 @@ import React from "react";
 import Image from "next/image";
 import { Link } from "@components/common/Link";
 import { Box, Typography } from "@mui/material";
-import { useTheme } from "@mui/material/styles";
 import treeIcon from "@public/assets/logos/bn_tree_only.svg";
 
 interface Props {
@@ -12,24 +11,20 @@ interface Props {
   color: string;
 }
 
-export const ContentLanguageSelector: FC<Props> = ({ title, href, color }) => {
-  const materialTheme = useTheme();
-
+export const DbLanguageLinkBox: FC<Props> = ({ title, href, color }) => {
   return (
     <Link
       href={href}
       sx={{
         display: "flex",
-        my: 2,
-        mx: 2,
         flexDirection: "column",
-        flex: 1,
+        flex: "0 0 auto",
+        width: {
+          xs: "calc(50% - 0.5rem)",
+          md: "160px",
+        },
         justifyContent: "center",
         alignItems: "center",
-        [materialTheme.breakpoints.down("sm")]: {
-          justifyContent: "flex-start",
-          flexDirection: "row",
-        },
         "&:hover": { opacity: 0.9 },
         "&:active": { opacity: 1 },
       }}
@@ -40,8 +35,8 @@ export const ContentLanguageSelector: FC<Props> = ({ title, href, color }) => {
         src={treeIcon}
         priority={false}
         sx={{
-          height: 120,
-          width: 120,
+          height: { xs: 100, sm: 130, md: 120 },
+          width: { xs: 100, sm: 130, md: 120 },
           p: 2,
           borderRadius: 2,
           background: color,
